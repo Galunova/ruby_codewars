f1548d15aef5eee552892abb8bc000c51bfebc94
Adelina Galunova <adelya@cloudcoreo.com>
2020-06-01 17:13:05 +0600

    Simple Encryption#1 With Zip

--- 6_kyu/57814d79a56c88e3e0000786/train.rb
+++ 6_kyu/57814d79a56c88e3e0000786/train.rb
@@ -14,11 +14,7 @@ def decrypt(encrypted_text, n)
 	n.times do
 		a = encrypted_text[0, encrypted_text.length/2]
 		b = encrypted_text.sub(a, '')
-		encrypted_text = ''
-		for i in 0..a.length-1
-			encrypted_text<<b[i]+a[i]
-		end
-		encrypted_text<<b[b.length-1] if b.length > a.length 
+		encrypted_text = b.chars.zip(a.chars).join
 	end
 	encrypted_text
 end
